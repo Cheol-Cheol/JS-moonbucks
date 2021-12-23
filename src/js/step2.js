e6f57609518789af2a592f0b06c8cccc653b7abe
Cheol-Cheol <dream174822@gmail.com>
2021-12-23 12:06:47 +0900

    ⚡️ 카테고리별 메뉴 수정, 삭제 그리고 리팩터링

--- src/js/step2.js
+++ src/js/step2.js
@@ -9,14 +9,14 @@
 
 // TODO 카테고리별 메뉴판 관리
 // - [x] 에스프레소 메뉴판 관리
-// - [] 프라푸치노 메뉴판 관리
-// - [] 블렌디드 메뉴판 관리
-// - [] 티바나 메뉴판 관리
-// - [] 디저트 메뉴판 관리
+// - [x] 프라푸치노 메뉴판 관리
+// - [x] 블렌디드 메뉴판 관리
+// - [x] 티바나 메뉴판 관리
+// - [x] 디저트 메뉴판 관리
 
 // TODO 페이지 접근시 최초 데이터 Read & Rendering
-// - [] 페이지에 최초로 로딩될 때 localStorage에 에스프레소 메뉴를 읽어온다.
-// - [] 에스프레소 메뉴를 페이지에 그려준다.
+// - [x] 페이지에 최초로 로딩될 때 localStorage에 에스프레소 메뉴를 읽어온다.
+// - [x] 에스프레소 메뉴를 페이지에 그려준다.
 
 // TODO 품절 상태 관리
 // - [] 품절 버튼을 추가한다.
@@ -48,7 +48,9 @@ function App() {
     teavana: [],
     desert: [],
   };
+
   this.currentCategory = "espresso";
+
   this.init = () => {
     if (store.getLocalStorage()) {
       this.menu = store.getLocalStorage();
@@ -81,36 +83,36 @@ function App() {
     // template => ['<li>~</li>','<li>~</li>...'] 이런 배열 형태
     // 하나의 마크업으로 만들기 위해 문자열로 바꿔야한다. .join("") 추가해서 해결
     // '<li>~</li><li>~</li>...' 문자열 형태로 변경
-    $("#espresso-menu-list").innerHTML = template;
+    $("#menu-list").innerHTML = template;
     updateMenuCount();
   };
 
   const updateMenuCount = () => {
-    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+    const menuCount = $("#menu-list").querySelectorAll("li").length;
     // 변수명은 클래스명이나 요소의 힌트들을 사용해서 작명하자.
     $(".menu-count").innerText = `총 ${menuCount}개`;
   };
 
   const addMenuName = () => {
-    if ($("#espresso-menu-name").value === "") {
+    if ($("#menu-name").value === "") {
       alert("값을 입력해주세요");
       return;
     }
-    const espressoMenuName = $("#espresso-menu-name").value;
+    const menuName = $("#menu-name").value;
     // 1. this.menu(상태)에 메뉴들 추가하기
-    this.menu[this.currentCategory].push({ name: espressoMenuName });
+    this.menu[this.currentCategory].push({ name: menuName });
     // 2. this.menu를 LocalStorage에도 저장하기
     store.setLocalStorage(this.menu);
     // map를 통해 this.menu의 메뉴들 각각 마크업 시키기
     render();
-    $("#espresso-menu-name").value = "";
+    $("#menu-name").value = "";
   };
 
   const updateMenuName = (e) => {
     const menuId = e.target.closest("li").dataset.menuId;
     const $menuName = e.target.closest("li").querySelector(".menu-name");
     const updatedMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
-    this.menu[menuId].name = updatedMenuName;
+    this.menu[this.currentCategory][menuId].name = updatedMenuName;
     store.setLocalStorage(this.menu);
     $menuName.innerText = updatedMenuName;
   };
@@ -118,7 +120,7 @@ function App() {
   const removeMenuName = (e) => {
     if (confirm("정말 삭제하시겠습니까?")) {
       const menuId = e.target.closest("li").dataset.menuId;
-      this.menu.splice(menuId, 1);
+      this.menu[this.currentCategory].splice(menuId, 1);
       store.setLocalStorage(this.menu);
       e.target.closest("li").remove();
       // .remove() : DOM element를 삭제해주는 메소드
@@ -128,7 +130,7 @@ function App() {
 
   // 이벤트 함수들
   // 이벤트 위임 - e.target
-  $("#espresso-menu-list").addEventListener("click", (e) => {
+  $("#menu-list").addEventListener("click", (e) => {
     if (e.target.classList.contains("menu-edit-button")) {
       // classList.contains() & .closest()
       updateMenuName(e);
@@ -140,14 +142,14 @@ function App() {
   });
 
   // form태그가 자동으로 전송되는 것을 막아준다.
-  $("#espresso-menu-form").addEventListener("submit", (e) => {
+  $("#menu-form").addEventListener("submit", (e) => {
     e.preventDefault();
   });
 
-  $("#espresso-menu-submit-button").addEventListener("click", addMenuName);
+  $("#menu-submit-button").addEventListener("click", addMenuName);
 
   // 메뉴의 이름을 입력받는건
-  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+  $("#menu-name").addEventListener("keypress", (e) => {
     if (e.key !== "Enter") {
       return;
     }
@@ -158,7 +160,9 @@ function App() {
     const isCategoryButton = e.target.classList.contains("cafe-category-name");
     if (isCategoryButton) {
       const categoryName = e.target.dataset.categoryName;
-      console.log(categoryName);
+      this.currentCategory = categoryName;
+      $("#category-title").innerText = `${e.target.innerText} 메뉴 관리`;
+      render();
     }
   });
 }
